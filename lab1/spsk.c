e504b4d6b68b3b0d102d8acf5b32acf20b7de7fc
Max-Sam231 <maxmarutin64649@gmail.com>
2025-03-06 15:41:32 +0600

    feat: add list

--- lab1/spsk.c
+++ lab1/spsk.c
@@ -1,1 +1,90 @@
-// список
+#include "spsk.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Item *CreateItem(int data) {
+  Item *newItem = (Item *)malloc(sizeof(Item));
+  newItem->current = data;
+  newItem->next = NULL;
+  return newItem;
+}
+
+void PushStart(Item **startElem, int data) {
+  Item *newItem = CreateItem(data);
+  newItem->next = *startElem;
+  *startElem = newItem;
+}
+
+void PushEnd(Item **startElem, int data) {
+  Item *newItem = CreateItem(data);
+  if (*startElem == NULL) {
+    *startElem = newItem;
+    return;
+  }
+  Item *tempStartElem = *startElem;
+  while (tempStartElem->next != NULL) {
+    tempStartElem = tempStartElem->next;
+  }
+  tempStartElem->next = newItem;
+}
+
+void PushMiddle(Item **startElem, int data, int index) {
+  if (index <= 0) {
+    printf("Неверный индекс");
+  }
+  Item *newItem = CreateItem(data);
+  Item *tempStartElem = *startElem;
+  for (int i = 0; i < index - 1; i++) {
+    if (tempStartElem == NULL) {
+      printf("Неверный индекс");
+      return;
+    }
+    tempStartElem = tempStartElem->next;
+  }
+  newItem->next = tempStartElem->next;
+  tempStartElem->next = newItem;
+}
+
+void PopItem(Item **startElem, int value) {
+  Item *tempStartElem = *startElem;
+  Item *prev = NULL;
+  if (tempStartElem != NULL && tempStartElem->current == value) {
+    *startElem = tempStartElem->next;
+    free(tempStartElem);
+    return;
+  }
+  while (tempStartElem != NULL && tempStartElem->current != value) {
+    prev = tempStartElem;
+    tempStartElem = tempStartElem->next;
+  }
+  if (tempStartElem == NULL) {
+    printf("Элемент не найден\n");
+    return;
+  }
+  prev->next = tempStartElem->next;
+  free(tempStartElem);
+}
+
+Item *FindItem(Item *startElem, int index) {
+  int count = 0;
+  Item *tempStartElem = startElem;
+  while (tempStartElem != NULL) {
+    if (count == index) {
+      return tempStartElem;
+    }
+    count++;
+    tempStartElem = tempStartElem->next;
+  }
+  return NULL;
+}
+
+int CountElem(Item *startElem) {
+  int count = 0;
+  Item *tempStartElem = startElem;
+  while (tempStartElem != NULL) {
+    count++;
+    tempStartElem = tempStartElem->next;
+  }
+  return count;
+}
--- lab1/tests/spsk_test.c
+++ lab1/tests/spsk_test.c
@@ -1,4 +1,149 @@
 #include "../spsk.h"
+#include <assert.h>
 #include <stdio.h>
 
-int main() { return 0; }
+void test_CreateItem() {
+  Item *item = CreateItem(42);
+  assert(item != NULL);
+  assert(item->current == 42);
+  assert(item->next == NULL);
+  free(item);
+  printf("test_CreateItem passed!\n");
+}
+
+void test_PushStart() {
+  Item *list = NULL;
+  PushStart(&list, 10);
+  assert(list != NULL);
+  assert(list->current == 10);
+  assert(list->next == NULL);
+
+  PushStart(&list, 20);
+  assert(list->current == 20);
+  assert(list->next->current == 10);
+  assert(list->next->next == NULL);
+
+  free(list->next);
+  free(list);
+  printf("test_PushStart passed!\n");
+}
+
+void test_PushEnd() {
+  Item *list = NULL;
+  PushEnd(&list, 10);
+  assert(list != NULL);
+  assert(list->current == 10);
+  assert(list->next == NULL);
+
+  PushEnd(&list, 20);
+  assert(list->current == 10);
+  assert(list->next->current == 20);
+  assert(list->next->next == NULL);
+
+  free(list->next);
+  free(list);
+  printf("test_PushEnd passed!\n");
+}
+
+void test_PushMiddle() {
+  Item *list = NULL;
+  PushEnd(&list, 10);
+  PushEnd(&list, 30);
+  PushMiddle(&list, 20, 1);
+
+  assert(list->current == 10);
+  assert(list->next->current == 20);
+  assert(list->next->next->current == 30);
+  assert(list->next->next->next == NULL);
+
+  free(list->next->next);
+  free(list->next);
+  free(list);
+  printf("test_PushMiddle passed!\n");
+}
+
+void test_PopItem() {
+  Item *list = NULL;
+  PushEnd(&list, 10);
+  PushEnd(&list, 20);
+  PushEnd(&list, 30);
+
+  PopItem(&list, 20);
+  assert(list->current == 10);
+  assert(list->next->current == 30);
+  assert(list->next->next == NULL);
+
+  PopItem(&list, 10);
+  assert(list->current == 30);
+  assert(list->next == NULL);
+
+  PopItem(&list, 30);
+  assert(list == NULL);
+
+  free(list);
+  printf("test_PopItem passed!\n");
+}
+
+void test_FindItem() {
+  Item *list = NULL;
+  PushEnd(&list, 10);
+  PushEnd(&list, 20);
+  PushEnd(&list, 30);
+
+  Item *foundItem = FindItem(list, 1);
+  assert(foundItem != NULL);
+  assert(foundItem->current == 20);
+
+  foundItem = FindItem(list, 5);
+  assert(foundItem == NULL);
+
+  free(list->next->next);
+  free(list->next);
+  free(list);
+  printf("test_FindItem passed!\n");
+}
+
+void test_CountElem() {
+  Item *list = NULL;
+  assert(CountElem(list) == 0);
+
+  PushEnd(&list, 10);
+  assert(CountElem(list) == 1);
+
+  PushEnd(&list, 20);
+  PushEnd(&list, 30);
+  assert(CountElem(list) == 3);
+
+  free(list->next->next);
+  free(list->next);
+  free(list);
+  printf("test_CountElem passed!\n");
+}
+
+void test_PopItem_NotFound() {
+  Item *list = NULL;
+  PushEnd(&list, 10);
+  PushEnd(&list, 20);
+
+  PopItem(&list, 30); // Элемент не найден
+  assert(list->current == 10);
+  assert(list->next->current == 20);
+  assert(list->next->next == NULL);
+
+  free(list->next);
+  free(list);
+  printf("test_PopItem_NotFound passed!\n");
+}
+
+int main() {
+  test_CreateItem();
+  test_PushStart();
+  test_PushEnd();
+  test_PushMiddle();
+  test_PopItem();
+  test_FindItem();
+  test_CountElem();
+  test_PopItem_NotFound();
+
+  return 0;
+}
