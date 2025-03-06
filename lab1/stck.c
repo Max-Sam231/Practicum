34026cb7f08454cc661b5c30d1e9d88635606bae
Max-Sam231 <maxmarutin64649@gmail.com>
2025-03-06 12:23:33 +0600

    feat: add stack

--- lab1/stck.c
+++ lab1/stck.c
@@ -1,1 +1,36 @@
-// стэк
+#include "stck.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+void Init(Stack *stack) { stack->topElem = -1; };
+
+bool Empty(Stack *stack) { return stack->topElem == -1; };
+
+bool Full(Stack *stack) { return stack->topElem == SIZE - 1; };
+
+int Peek(Stack *stack) {
+  if (Empty(stack) == false) {
+    printf("%d\n", stack->items[stack->topElem]);
+    return stack->items[stack->topElem];
+  } else {
+    printf("Стэк пуст\n");
+    return -1;
+  }
+}
+
+void Push(Stack *stack, int value) {
+  if (Full(stack)) {
+    printf("Стэк переполнен\n");
+    return;
+  }
+  stack->topElem++;
+  stack->items[stack->topElem] = value;
+};
+
+void Pop(Stack *stack) {
+  if (Empty) {
+    printf("Стэк пуст\n");
+    return;
+  }
+  stack->topElem--;
+};
--- lab1/stck.h
+++ lab1/stck.h
@@ -1,1 +1,15 @@
-// стэк
+#include <stdbool.h>
+
+#define SIZE 10
+
+typedef struct {
+  int items[SIZE];
+  int topElem;
+} Stack;
+
+void Init(Stack *stack);
+bool Empty(Stack *stack);
+bool Full(Stack *stack);
+int Peek(Stack *stack);
+void Push(Stack *stack, int value);
+void Pop(Stack *stack);
